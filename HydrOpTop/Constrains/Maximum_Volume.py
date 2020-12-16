Add density filter. Work in the optimization but max_vol constrain not working properly (wrong value of volume fraction)

HydrOpTop/Constrains/Maximum_Volume.py
@@ -20,7 +20,11 @@ class Maximum_Volume:
     self.max_v_frac = max_volume_percentage
     self.V = None
     self.V_tot = None
-    self.p_duplicate = None
+    self.filter = None
+    return
+  
+  def set_filter(self, filter):
+    self.filter = filter
     return
   
   def set_inputs(self, inputs):
@@ -33,7 +37,12 @@ class Maximum_Volume:
     return
   
   def evaluate(self, p, grad):
-    self.p[self.p_cell_ids-1] = p
+    if self.filter:
+      p_bar = self.filter.get_filtered_density(p)
+    else:
+      p_bar = p
+    self.p[self.p_cell_ids-1] = p_bar #self.p spand over all the domain, 
+                                  #but p_bar only on the cell id to optimize
     if self.ids_to_consider is None:
       V_tot = np.sum(self.V)
       constrain = np.sum(self.V*self.p)/V_tot - self.max_v_frac
@@ -45,8 +54,11 @@ class Maximum_Volume:
         grad[:] = self.V/V_tot
       else:
         grad[:] = self.V[self.ids_to_consider-1]/V_tot
+      if self.filter:
+        grad[:] = self.filter.get_filter_derivative(p).transpose().dot(grad)
     print(f"Current volume constrain: {constrain+self.max_v_frac}")
     return constrain
+  
   
   def __get_PFLOTRAN_output_variable_needed__(self):
     return ["VOLUME"]

HydrOpTop/Crafter/Steady_State_Crafter.py
@@ -15,29 +15,36 @@ class Steady_State_Crafter:
   - objectif: the objective function (Objectif class instance)
   - constrains: a list of constrains (Constrain class instances
   - coupling: specify how each material properties should be optimized
-                       (coupled = one unique p parameter per cell, half = coupled
-                       for duplicate ids to optimize in each material, none =
-                       each material properties have a separate parameter) 
-                       (default=total)
+              (coupled = one unique p parameter per cell, half = coupled
+              for duplicate ids to optimize in each material, none =
+              each material properties have a separate parameter) 
+              (default=total)
+  - filter: the filter to be used to relate the density to a filtered density
+            (a fitler class instance) (None by default).
   """
-  def __init__(self, objectif, solver, mat_props, constrains, coupling="total"):
+  def __init__(self, objectif, solver, mat_props, constrains, filter=None, coupling="total"):
     self.mat_props = mat_props
     self.solver = solver
     self.obj = objectif
     self.constrains = constrains
+    self.filter = filter
     self.coupling = coupling
     
     #self.Xi = None #store material properties
     self.adjoint = None
+    self.filter_i = None #filter inputs
     self.Yi = None #store PFLOTRAN output
     self.p_ids = None
+    self.d_pbar_dp = None #derivative of filtered density to raw density
     
     #option
     self.print_every = 0
     self.print_every_out = "p.h5"
     
     self.__initialize_IO_array__()
-    self.first_call = True
+    self.__initialize_filter__()
+    self.first_call_evaluation = True
+    self.first_call_gradient = True
     self.func_eval = 0
     self.last_p = None
     self.adjoint_algo = None
@@ -57,14 +64,22 @@ class Steady_State_Crafter:
   
   
   
-  
   def evaluate_objective(self, p):
+    ###FILTERING: convert p to p_bar
+    if self.filter is None:
+      p_bar = p
+    else: 
+      for i,var in enumerate(self.filter.__get_PFLOTRAN_output_variable_needed__()):
+        self.solver.get_output_variable(var, self.filter_i[i], -1) #last timestep
+      p_bar = self.filter.get_filtered_density(p)
+    
     ### UPDATE MAT PROPERTIES AND RUN PFLOTRAN ###
     #Given p, update material properties
     for mat_prop in self.mat_props:
-      X = mat_prop.convert_p_to_mat_properties(p)
+      X = mat_prop.convert_p_to_mat_properties(p_bar)
       self.solver.create_cell_indexed_dataset(X, mat_prop.get_name().lower(),
                     X_ids=mat_prop.get_cell_ids_to_parametrize(), resize_to=True)
+    
     #run PFLOTRAN
     ret_code = self.solver.run_PFLOTRAN()
     if ret_code: return np.nan
@@ -84,27 +99,34 @@ class Steady_State_Crafter:
     # note that we have in place assignement, so we don't have to
     # update the Yi in the objective
     cf = self.obj.evaluate()
+    
+    self.first_call_evaluation = False
     return cf
   
   
   
   def compute_gradient(self, p, grad=None):
-    #note: the evaluate_objective function should have been must 
-    # have been called before
+    #note: the evaluate_objective function must have been called before
+    if self.func_eval < 1:
+      print("Objective function must have called before computing gradient")
+      exit(1)
     if grad is None:
       grad = np.zeros(len(self.p_ids),dtype='f8')
+      
     ### CREATE ADJOINT ###
-    if self.first_call:
-      self.fist_call = False
+    if self.first_call_gradient:
       self.__initialize_adjoint__()
+      
     ### UPDATE ADJOINT ###
     #cost derivative to pressure (vector)
     self.obj.d_objective_dP(self.adjoint.dc_dP)
+    
     #cost derivative to mat properties (vector)
     for i,mat_prop in enumerate(self.mat_props):
       var = mat_prop.get_name()
       if self.obj.__depend_of_mat_props__(var):
-        self.obj.d_objective_d_inputs(var, self.adjoint.dc_dXi[i].data)
+        self.obj.d_objective_d_inputs(var, self.adjoint.dc_dXi[i])
+        
     #update matrix
     #note the I,J do not change, only the data
     #residual according to pressure
@@ -117,8 +139,14 @@ class Steady_State_Crafter:
     for i,mat_prop in enumerate(self.mat_props):
       mat_prop.d_mat_properties(p, self.adjoint.dXi_dp[i].data)
 
-    ### COMPUTE ADJOINT ###
+    ### COMPUTE GRADIENT ###
     self.adjoint.compute_sensitivity(grad, assign_at_ids=self.p_ids)
+    
+    ### DERIVE ACCORDING TO FILTER ###
+    if self.filter:
+      self.filter.get_filter_derivative(p, self.d_pbar_dp)
+      grad[:] = self.d_pbar_dp.transpose().dot(grad)
+    self.first_call_gradient = False
     return grad
     
     
@@ -141,6 +169,7 @@ class Steady_State_Crafter:
   
   
   def __initialize_IO_array__(self):
+    print("Initialization...")
     #create material parameter p
     if self.coupling == "total":
       #verify if each ids are the same
@@ -153,7 +182,7 @@ class Steady_State_Crafter:
             exit(1)
       if X is None: 
         self.problem_size = self.solver.n_cells
-        self.p_ids = np.arange(1, self.problem_size+1)
+        self.p_ids = np.arange(1, self.problem_size+1) #in pflotran indexing
       else: 
         self.problem_size = len(X)
         self.p_ids = X
@@ -176,16 +205,54 @@ class Steady_State_Crafter:
       constrain.set_inputs(self.constrain_arrays[-i-1:])
       if constrain.__need_p_cell_ids__():
         constrain.set_p_cell_ids(self.p_ids)
+        constrain.set_filter(self.filter)
+    return
+    
+  
+  def __initialize_filter__(self):
+    print("Filter initialization")
+    #filter initialization is tricky, because it may need PFLOTRAN output variable
+    # that have not been created yet. Thus, for instance, we have to run
+    # PFLOTRAN one time to initialize the filter (even if it's costly)...
+    if self.filter is None:
+      return
+      
+    self.filter.set_p_ids(self.p_ids)
+    n_inputs = len(self.filter.__get_PFLOTRAN_output_variable_needed__())
+    
+    #TODO: refactor below as I don't like to make one iteration just for
+    # initalize the filter
+    if n_inputs > 0:
+      #run pflotran to get its output
+      
+      #Given p, update material properties
+      p_bar = np.zeros(len(self.p_ids), dtype='f4')
+      for mat_prop in self.mat_props:
+        X = mat_prop.convert_p_to_mat_properties(p_bar)
+        self.solver.create_cell_indexed_dataset(X, mat_prop.get_name().lower(),
+                      X_ids=mat_prop.get_cell_ids_to_parametrize(), resize_to=True)
+      #run PFLOTRAN
+      ret_code = self.solver.run_PFLOTRAN() #TODO: uncomment
+    
+    self.filter_i = [np.zeros(self.solver.n_cells, dtype='f8') for x in range(n_inputs)]
+    for i,var in enumerate(self.filter.__get_PFLOTRAN_output_variable_needed__()):
+      self.solver.get_output_variable(var, self.filter_i[i], -1) #last timestep
+    self.filter.set_inputs(self.filter_i)
+    self.filter.initialize()
+    self.d_pbar_dp = self.filter.get_filter_derivative(p_bar)
     return
   
   
   
   def __initialize_adjoint__(self):
+    #deferred initialization because we need the structure of the jacobian matrix
+    # created after PFLOTRAN first call
     #cost derivative to pressure (vector)
     dc_dP = np.zeros(self.solver.n_cells, dtype='f8')
     #cost derivative to mat properties (vector)
     if self.obj.__depend_of_mat_props__(): 
-      dc_dXi = np.zeros(self.solver.n_cells, dtype='f8')
+      dc_dXi = [np.zeros(self.solver.n_cells, dtype='f8') for x in 
+                                 self.obj.__depend_of_mat_props__()]
     else:
       dc_dXi = None
     #mat properties derivative to parameter (diag matrix)

HydrOpTop/PFLOTRAN.py
@@ -16,8 +16,7 @@ class PFLOTRAN:
     self.pft_in = pft_in
     self.input_folder = '/'.join(pft_in.split('/')[:-1])+'/'
     if self.input_folder[0] == '/': self.input_folder = '.' + self.input_folder
-    self.__read_input_file__()
-    self.__parse_regions__()
+    self.__get_input_deck__(self.input_folder + self.pft_in )
     self.__get_mesh_info__()
     self.__get_nvertices_ncells__()
     
@@ -50,6 +49,10 @@ class PFLOTRAN:
   
   # interacting with data #
   def get_region_ids(self, reg_name):
+    """
+    Return the cell ids associated to the given region:
+    - reg_name: the name of the region to get the ids.
+    """
     #look for region in pflotran input
     filename = ""
     for i,line in enumerate(self.input_deck):
@@ -76,6 +79,39 @@ class PFLOTRAN:
         src.close()
         print(f"Region not found in mesh file {filename}")
         exit(1)
+    return cell_ids
+  
+  def get_connections_ids_integral_flux(self, integral_flux_name):
+    """
+    Return the cell ids associated to the given integral flux
+    Argument:
+    - integral_flux_name: the name of the integral flux to get the ids.
+    """
+    found = False
+    #find the integral flux in input deck
+    for i,line in enumerate(self.input_deck):
+      if "INTEGRAL_FLUX" in line:
+        if integral_flux_name in line: 
+          found = True
+          break
+    if not found:
+      print(f"Integral flux {integral_flux_name} not found in input deck")
+      print("Please provide the name directly after the INTEGRAL_FLUX opening card")
+      exit(1)
+    #check if defined by cell ids
+    while "CELL_IDS" in line:
+      i += 1
+      line = self.input_deck[i]
+      for x in ["POLYGON", "COORDINATES_AND_DIRECTIONS", 
+                "PLANE", "VERTICES", "END", "/"]:
+        if x in line:
+          print("Only INTEGRAL_FLUX defined with CELL_IDS are supported at this time")
+          exit(1)
+    cell_ids = []
+    while "/" in line or "END" in line:
+      i += 1
+      line = self.input_deck[i].split()
+      cell_ids.append([line[0],line[1]])
     return cell_ids
   
   def create_cell_indexed_dataset(self, X_dataset, dataset_name, h5_file_name="",
@@ -142,6 +178,14 @@ class PFLOTRAN:
     - timestep: the i-th timestep to extract
     """
     src = h5py.File(self.pft_out, 'r')
+    #treat coordinate separately as they are in Domain/XC
+    if var in ["XC", "YC", "ZC"]:
+      if out is None:
+        out = np.array(src["Domain/"+var])
+      else:
+        out[:] = np.array(src["Domain/"+var])
+      src.close()
+      return out
     timesteps = [x for x in src.keys() if "Time" in x]
     right_time = timesteps[i_timestep]
     key_to_find = self.dict_var_out[var]
@@ -158,6 +202,7 @@ class PFLOTRAN:
       out = np.array(src[right_time + '/' + out_var])
     else:
       out[:] = np.array(src[right_time + '/' + out_var])
+    src.close()
     return out
   
   def get_sensitivity(self, var):
@@ -182,13 +227,33 @@ class PFLOTRAN:
   
   
   ### PRIVATE METHOD ###
-  def __read_input_file__(self):
+  def __get_input_deck__(self, filename):
+    self.input_deck = []
+    self.__read_input_file__(filename)
+    finish = False
+    while not finish:
+      for i,line in enumerate(self.input_deck):
+        if "EXTERNAL_FILE" in line:
+          line = line.split()
+          index = line.index("EXTERNAL_FILE")
+          self.__read_input_file__(line[index+1])
+          break
+      finish = True
+    return
+  
+  def __read_input_file__(self, filename, append_at_pos=0):
     """
     Store input deck
     """
-    input_file = self.input_folder + self.pft_in 
-    src = open(input_file, 'r')
-    self.input_deck = [x for x in src.readlines() if x]
+    src = open(filename, 'r')
+    temp = []
+    for line in src.readlines():
+      line = line.split('#')[0][:-1] #remove commentary and \n
+      if line: temp.append(line) 
+    if not self.input_deck: self.input_deck = temp
+    else:
+      self.input_deck = self.input_deck[:append_at_pos] + \
+                        temp + self.input_deck[append_at_pos:]
     src.close()
     return 
     
@@ -239,9 +304,6 @@ class PFLOTRAN:
       src.close()
       self.n_vertices = -1 #no info about it...
       return
-    
-  def __parse_regions__(self):
-    return
     
     
     