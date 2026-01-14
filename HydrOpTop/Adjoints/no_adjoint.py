[FIX] Fix adjoint calculation with a new application where property can be cell based

HydrOpTop/Adjoints/no_adjoint.py
@@ -4,22 +4,22 @@ class No_Adjoint:
   """
   A dummy adjoint class that solve no adjoint...
   """
-  def __init__(self, parametrized_mat_props, p_ids):
-    self.assign_at_ids = p_ids #in solver format!
+  def __init__(self, parametrized_mat_props, p_ids, ids_p):
+    self.p_ids = p_ids #in solver format!
+    self.ids_p = ids_p
     self.initialized = False
     self.parametrized_mat_props = parametrized_mat_props
     self.l0 = None
     
     self.dXi_dp = {m.get_name():None for m in parametrized_mat_props} # dim = [mat_prop] * L * T2 / M
     return
-  
+
   def update_mat_derivative(self, p):
-    for mat_prop in self.parametrized_mat_props:
-      mat_prop.d_mat_properties(
-        p, self.dXi_dp[mat_prop.get_name()]
-      )
+    for i,m in enumerate(self.parametrized_mat_props):
+      indexes = self.ids_p[m.cell_ids]
+      self.dXi_dp[m.get_name()][indexes] = m.d_mat_properties(p[indexes])
     return
-    
+
   def compute_sensitivity(self, func, filter_sequence, p):
     """
     Compute the total cost function derivative according to material density
@@ -35,28 +35,33 @@ class No_Adjoint:
     p_bar, Jf = filter_sequence.filter_derivative(p)
 
     #create and update structures
-    if self.initialized == False: self.__initialize_adjoint__(p)
-    else: self.update_mat_derivative(p)
+    if self.initialized == False: self.__initialize_adjoint__(p_bar)
+    self.update_mat_derivative(p_bar)
 
+    # in dobj_dX, the function ask only the p_bar for its indexes
+    p_bar_ = np.zeros( max(func.indexes.max(),self.p_ids.max())+1 ) + np.nan
+    p_bar_[self.p_ids] = p_bar
+    p_bar_ = p_bar_[func.indexes]
     dobj_dX = {}
     for var in func.__get_variables_needed__():
       # If not solved
       if var in [x.get_name() for x in self.parametrized_mat_props]:
-        dfunc = func.d_objective(var, p_bar)
+        dfunc = func.d_objective(var, p_bar_)
         dobj_dX[var] = np.zeros(self.solver.get_system_size())
         if dfunc.ndim == 2: dobj_dX[var] = dobj_dX[var].repeat(dfunc.shape[1]).reshape(self.solver.get_system_size(),dfunc.shape[1])
         dobj_dX[var][func.indexes-self.solver.cell_id_start_at] = dfunc
 
-    dobj_dp_partial = func.d_objective_dp_partial(p_bar)
+    dobj_dp_partial = np.zeros_like(p_bar)
+    dobj_dp_partial[self.ids_p[func.indexes]] = func.d_objective_dp_partial(p_bar_)
 
     dc_dXi_dXi_dp = 0.
     for name in dobj_dX.keys():
-      dc_dXi_dXi_dp += dobj_dX[name][self.assign_at_ids-1]*self.dXi_dp[name]
-    return dc_dXi_dXi_dp + dobj_dp_partial
+      dc_dXi_dXi_dp += dobj_dX[name][self.p_ids] * self.dXi_dp[name]
+    return (dc_dXi_dXi_dp + dobj_dp_partial) @ Jf
   
-  def __initialize_adjoint__(self,p): 
+  def __initialize_adjoint__(self,p):
     self.dXi_dp = {
-      mat_prop.get_name():mat_prop.d_mat_properties(p) for mat_prop in self.parametrized_mat_props
+      mat_prop.get_name():np.zeros_like(p) for mat_prop in self.parametrized_mat_props
     }
     self.initialized = True
     return

HydrOpTop/Adjoints/sensitivity_steady_simple.py
@@ -22,7 +22,7 @@ class Sensitivity_Steady_Simple:
   If cost_deriv_mat_prop is None, assume the cost function does not depend on
   the material property distribution.
   """
-  def __init__(self, solved_vars, parametrized_mat_props, solver, p_ids, solver_args={}):
+  def __init__(self, solved_vars, parametrized_mat_props, solver, p_ids, ids_p, solver_args={}):
     
     #vector
     #self.dc_dP = cost_deriv_pressure #dim = [cost] * L * T2 / M
@@ -30,7 +30,8 @@ class Sensitivity_Steady_Simple:
     self.solved_vars = solved_vars
     self.parametrized_mat_props = parametrized_mat_props
     self.solver = solver
-    self.assign_at_ids = p_ids #in solver format!
+    self.p_ids = p_ids #in solver format!
+    self.ids_p = ids_p #in solver format!
     
     solver_args_ = DEFAULT_SOLVER_ARGS.copy()
     solver_args_.update(solver_args)
@@ -51,7 +52,8 @@ class Sensitivity_Steady_Simple:
     
   def update_mat_derivative(self, p):
     for i,m in enumerate(self.parametrized_mat_props):
-      self.dXi_dp[m.get_name()][m.indexes] = m.d_mat_properties(p[m.indexes])
+      indexes = self.ids_p[m.cell_ids]
+      self.dXi_dp[m.get_name()][indexes] = m.d_mat_properties(p[indexes])
     return
   
   def update_residual_derivatives(self):
@@ -86,32 +88,37 @@ class Sensitivity_Steady_Simple:
     if self.initialized == False:
       self.__initialize_adjoint__(p_bar)
     else:
-      self.update_mat_derivative(p_bar)
       self.update_residual_derivatives()
+    self.update_mat_derivative(p_bar)
 
     # Get derivative of the cost function with p_bar
     # derivative according to solved var
+    # in dobj_dXY, the function ask only the p_bar for its indexes
+    p_bar_ = np.zeros( max(func.indexes.max(),self.p_ids.max())+1 ) + np.nan
+    p_bar_[self.p_ids] = p_bar
+    p_bar_ = p_bar_[func.indexes]
     dobj_dY = {}
     dobj_dX = {}
     # Loop over the function variables
     for var in func.__get_variables_needed__():
       # If a solved variable, this go to the adjoint
       if var in self.solver.solved_variables:
-        dfunc = func.d_objective(var, p_bar)
+        dfunc = func.d_objective(var, p_bar_)
         dobj_dY[var] = np.zeros(self.solver.get_system_size())
         if dfunc.ndim == 2: dobj_dY[var] = dobj_dY[var].repeat(dfunc.shape[1]).reshape(self.solver.get_system_size(),dfunc.shape[1])
         dobj_dY[var][func.indexes-self.solver.cell_id_start_at] = dfunc
       # If not solved
       elif var in [x.get_name() for x in self.parametrized_mat_props]:
-        dfunc = func.d_objective(var, p_bar)
+        dfunc = func.d_objective(var, p_bar_)
         dobj_dX[var] = np.zeros(self.solver.get_system_size())
         if dfunc.ndim == 2: dobj_dX[var] = dobj_dX[var].repeat(dfunc.shape[1]).reshape(self.solver.get_system_size(),dfunc.shape[1])
         dobj_dX[var][func.indexes-self.solver.cell_id_start_at] = dfunc
     
-    dobj_dp_partial = func.d_objective_dp_partial(p_bar)
-    # dobj_dp_partial = np.zeros(self.solver.get_system_size())
+    dfunc = func.d_objective_dp_partial(p_bar_)
+    dobj_dp_partial = np.zeros_like(p_bar)
+    if np.any(dfunc):
+      dobj_dp_partial[self.ids_p[func.indexes]] = dfunc
     # if dfunc.ndim == 2: dobj_dp_partial = dobj_dp_partial.repeat(dfunc.shape[1]).reshape(self.solver.get_system_size(),dfunc.shape[1])
-    # dobj_dp_partial[func.indexes] = dfunc
 
     #note: dR_dXi in solver ordering, so we convert it to p ordering with assign_at_ids
     # and dXi_dP in p ordering
@@ -119,7 +126,7 @@ class Sensitivity_Steady_Simple:
     assert len(self.dR_dXi) == len(self.dXi_dp)
     var = [x for x in self.dXi_dp.keys()][0]
     n = len(self.dXi_dp[var])
-    dR_dXi_dXi_dp = ( (self.dR_dXi[var]).tocsr() )[:,self.assign_at_ids].dot(
+    dR_dXi_dXi_dp = ( (self.dR_dXi[var]).tocsr() )[:,self.p_ids].dot(
       dia_matrix( (self.dXi_dp[var],[0]), shape=(n,n) )
     )
 
@@ -137,7 +144,7 @@ class Sensitivity_Steady_Simple:
     
     dc_dXi_dXi_dp = 0.
     for name in dobj_dX.keys():
-      dc_dXi_dXi_dp += dobj_dX[name][self.assign_at_ids]*self.dXi_dp[name]
+      dc_dXi_dXi_dp += dobj_dX[name][self.p_ids]*self.dXi_dp[name]
 
     #if self.assign_at_ids is not None and isinstance(dc_dXi_dXi_dp,np.ndarray):
     #  dc_dXi_dXi_dp = dc_dXi_dXi_dp[self.assign_at_ids-1]
@@ -150,26 +157,23 @@ class Sensitivity_Steady_Simple:
     A = self.dR_dYi[var].tocsr()[keep][:,keep]
     b = dobj_dY[var][keep]
     self.l0 = self.adjoint.solve(A, b) #solver ordering
-    S = dc_dXi_dXi_dp - (R_mat.transpose().tocsc()[:,keep]).dot(self.l0)
+    # const terms
+    S = - (R_mat.transpose().tocsc()[:,keep]).dot(self.l0)
 
     if isinstance(S, np.ndarray) and S.ndim == 2:
-      S += dobj_dp_partial[:,None]
+      S += (dc_dXi_dXi_dp + dobj_dp_partial[:,None]) @ Jf
     else:
-      S += dobj_dp_partial
+      S += (dc_dXi_dXi_dp + dobj_dp_partial) @ Jf
 
     return S
   
   
   def __initialize_adjoint__(self,p_bar):
-    self.dXi_dp = {}
-    mat_prop_unique = set([m.get_name() for m in self.parametrized_mat_props])
-    for mu in mat_prop_unique:
-      dXi_dp = np.zeros_like(p_bar)
-      for m in self.parametrized_mat_props:
-        if m.get_name() != mu: continue
-        dXi_dp[m.indexes] = m.d_mat_properties(p_bar[m.indexes])
-      self.dXi_dp[mu] = dXi_dp
+    self.dXi_dp = {
+      mat_prop.get_name():np.zeros_like(p_bar) for mat_prop in self.parametrized_mat_props
+    }
 
+    mat_prop_unique = set([m.get_name() for m in self.parametrized_mat_props])
     self.dR_dXi = {
       mu : self.solver.get_sensitivity(mu) for mu in mat_prop_unique
     }

HydrOpTop/Crafter/steady_state_crafter.py
@@ -165,7 +165,8 @@ class Steady_State_Crafter:
       X = np.zeros_like(p_)
       for mat_prop in self.mat_props:
         if mat_prop.get_name() != mu: continue
-        X[mat_prop.indexes] = mat_prop.convert_p_to_mat_properties(p_[mat_prop.indexes])
+        indexes = self.ids_p[mat_prop.cell_ids]
+        X[indexes] = mat_prop.convert_p_to_mat_properties(p_[indexes])
       ret[mu] = X
     return ret
     
@@ -715,7 +716,7 @@ class Steady_State_Crafter:
       if var_ in self.solver.solved_variables:
         solved_variables_needed.append(var_)
     if len(solved_variables_needed) == 0:
-      adjoint = No_Adjoint(self.mat_props, self.p_ids)
+      adjoint = No_Adjoint(self.mat_props, self.p_ids-self.solver.cell_id_start_at, self.ids_p)
     elif self.adjoint == "fd":
       f = lambda p: self.evaluate_objective(self.pre_evaluation_objective(p))
       adjoint = Sensitivity_Finite_Difference(f, **self.adjoint_args)
@@ -725,6 +726,7 @@ class Steady_State_Crafter:
         self.mat_props,
         self.solver,
         self.p_ids-self.solver.cell_id_start_at,
+        self.ids_p,
         *( (self.adjoint_args,) if self.adjoint_args is not None else () )
       )
     elif self.adjoint == "adjoint-corrected":

HydrOpTop/Functions/Base_Function_class.py
@@ -124,7 +124,7 @@ class Base_Function:
         Returns:
             np.ndarray: Partial derivative with respect to `p`.
         """
-        return np.array([0.])
+        return np.zeros_like(p)
 
 
     def output_to_user(self):

tests/units/derivatives/test_crafter_derivative.py
@@ -9,6 +9,8 @@ from HydrOpTop.Materials import Identity
 from HydrOpTop.Crafter import Steady_State_Crafter
 from HydrOpTop.Filters import Density_Filter, Heaviside_Filter, Zone_Homogeneous
 
+from utils import finite_difference_dp
+
 
 class Test_Crafter:
     
@@ -73,6 +75,29 @@ class Test_Crafter:
     # Comparison
     np.testing.assert_allclose(J_adjoint, J_ana, atol=1e-6, rtol=1e-5)
     return
+
+  def test_crafter_derivative_level0_fancy_param(self):
+    """
+    Test derivative against a non homogeneous parametrization (depend of a cell_id)
+    """
+    from HydrOpTop.Materials import SIMP
+    cell_ids = np.random.permutation(self.all_cells[1:5])
+    bounds = [np.random.random(4), 2.]
+    param = SIMP(cell_ids, "a", bounds)
+    solver = Dummy_Simulator(problem_size=self.N, seed=1234, start_at=1)
+    obj = Sum_Variable("x", ids_to_consider=solver.get_region_ids("__all__"))
+    craft = Steady_State_Crafter(
+        obj, solver, [param], filters=[], deriv="adjoint", deriv_args={"method":"direct"}
+    )
+    p = np.random.random(len(param.cell_ids))
+    # adjoint derivative
+    S_adjoint = craft.evaluate_total_gradient(obj, p)
+    # finite difference
+    S_fd = finite_difference_dp(lambda x: craft.evaluate_objective(craft.pre_evaluation_objective(x)), p)
+    # Comparison
+    np.testing.assert_allclose(S_adjoint, S_fd, atol=1e-6, rtol=1e-5)
+    return
+
 
   def test_crafter_derivative_level1_diag_filter(self):
     """
@@ -144,13 +169,32 @@ class Test_Crafter:
     craft.pre_evaluation_objective(p)
     S_adjoint = craft.evaluate_total_gradient(obj, p)
     # fd deriv
-    craft_fd = Steady_State_Crafter(
-        copy.deepcopy(obj), self.solver, [self.parametrization], filters=filters, deriv="fd", deriv_args={"scheme":"central","step":1e-6}
-    )
-    S_fd = craft_fd.evaluate_total_gradient(obj, p)
-
+    # we can't use standard FD in the crafter as no_adjoint is enforced when func does not depend of variable
+    S_fd = finite_difference_dp(lambda x: craft.evaluate_objective(craft.pre_evaluation_objective(x)), p)
     assert craft.get_problem_size() == 8 # 4 + 1 + 3
     np.testing.assert_allclose(S_adjoint, S_fd, atol=1e-6, rtol=1e-6)
+
+
+  def test_crafter_derivative_level3_multiple_param(self):
+    """
+    Test adjoint derivative with multiple different parametrization of the same variable
+    """
+    from HydrOpTop.Materials import Log_SIMP
+    param1 = Log_SIMP(np.random.permutation(self.all_cells[1:5]), "a", [1., 10.])
+    param2 = Log_SIMP(np.random.permutation(self.all_cells[5:8]), "a", [10., 100.])
+    solver = Dummy_Simulator(problem_size=self.N, seed=1234, start_at=1)
+    obj = Sum_Variable("x", ids_to_consider=solver.get_region_ids("__all__"))
+    craft = Steady_State_Crafter(
+        obj, solver, [param1,param2], filters=[], deriv="adjoint", deriv_args={"method":"direct"}
+    )
+    p = np.random.random(craft.get_problem_size())
+    # adjoint derivative
+    S_adjoint = craft.evaluate_total_gradient(obj, p)
+    # finite difference
+    S_fd = finite_difference_dp(lambda x: craft.evaluate_objective(craft.pre_evaluation_objective(x)), p)
+    # Comparison
+    np.testing.assert_allclose(S_adjoint, S_fd, atol=1e-6, rtol=1e-5)
+    return
 
 
   @pytest.mark.skip(reason="Not implemented")

tests/units/derivatives/utils.py
@@ -17,12 +17,13 @@ def finite_difference_dvar(f, var, p, eps=EPS):
         grad[i] = (f1 - f2) / (2 * eps)
     return grad
 
-def finite_difference_dp(f, p, eps=EPS):
+def finite_difference_dp(f, p, eps=EPS, i_der=[]):
     grad = np.zeros_like(p)
-    for i in range(len(p)):
+    if not i_der: i_der = range(len(p))
+    for i in i_der:
         x1 = p.copy()
         x2 = p.copy()
         x1[i] += eps
         x2[i] -= eps
-        grad[i] = (f.evaluate(x1) - f.evaluate(x2)) / (2 * eps)
+        grad[i] = (f(x1) - f(x2)) / (2 * eps)
     return grad