[FIX] Upgrade gradient function with new crafter behavior

HydrOpTop/Crafter/steady_state_crafter.py
@@ -658,16 +658,14 @@ class Steady_State_Crafter:
       self.__initialize_adjoint__(self.obj)
     if self.obj.indexes is None:
       # pass all cell
-      self.obj.indexes = np.arange(len(self.solver.get_region_ids("__all__")))
-      
-    #self.obj.set_p_to_cell_ids(self.p_ids)
+      self.obj.indexes = self.solver.get_region_ids("__all__")
+    self.obj.cell_id_start_at = self.solver.cell_id_start_at
     
     #initialize adjoint for constraints
     self.last_constraints = {x[0].name:0. for x in self.constraints}
     self.last_grad_constraints = {x[0].name:None for x in self.constraints}
     for constraint in self.constraints:
       constraint = constraint[0]
-      #constraint.set_p_to_cell_ids(self.p_ids)
       # set adjoint
       if constraint.adjoint is None:
         self.__initialize_adjoint__(constraint)

HydrOpTop/Filters/heaviside_filter.py
@@ -11,16 +11,16 @@ class Heaviside_Filter(Base_Filter):
     and to avoid blurry contour. The smooth Heaviside function is defined as:
 
     .. math::
-      
+
        \tilde{p}_i = \frac{\tanh(\beta \eta) + \tanh(\beta (\bar p - \eta))}
                           {\tanh(\beta \eta) + \tanh(\beta (1- \eta))}
 
-  Parameters: 
-    ``cutoff`` (float): the cutoff parameter :math:`\eta` (i.e. the value of 
+  Parameters:
+    ``cutoff`` (float): the cutoff parameter :math:`\eta` (i.e. the value of
     :math:`p_i` where the step is located)
-    
+
     ``steepness`` (float): the steepness of the smooth Heaviside function :math:`\beta`
-  
+
   Required solver output:
 
   """
@@ -33,24 +33,28 @@ class Heaviside_Filter(Base_Filter):
     self.initialized = False
     self.name = "Heaviside Filter"
     return
-  
+
   def get_filtered_density(self, p):
     a = np.tanh(self.stepness*self.cutoff)
     p_filtered = (a+np.tanh(self.stepness*(p-self.cutoff))) / \
                        (a+np.tanh(self.stepness*(1-self.cutoff)))
     return p_filtered
-  
+
   def get_filter_derivative(self, p):
     d_p_filtered = 1 - np.tanh(self.stepness*(p-self.cutoff))**2
     a = np.tanh(self.stepness*self.cutoff)
     d_p_filtered *= self.stepness / (a + np.tanh(self.stepness*(1-self.cutoff)))
     n = np.arange(len(d_p_filtered))
     return coo_matrix((d_p_filtered,(n,n)))
-  
+
+  def update_stepness(self,s):
+    self.stepness = s
+    return
+
   def plot_filtered_density(self):
     r"""
     Visualize the transformation applied
-    
+
     """
     try:
       import matplotlib.pyplot as plt

HydrOpTop/Functions/cell_gradient.py
@@ -3,32 +3,26 @@ from .Base_Function_class import Base_Function
 from .common import __cumsum_from_connection_to_array__
 
 
-class Head_Gradient(Base_Function):
+class Cell_Gradient(Base_Function):
   r"""
-  Calculate the mean head gradient in the prescribed domain:
+  Calculate the mean (cell-computed) gradient of the given variable in the prescribed domain:
 
   .. math::
-     
-      f = \frac{1}{V_D} \sum_{i \in D} V_i ||\nabla {h_i} ||^n
 
-  Head is defined as:
-
-  .. math::
-       
-      h_i = \frac{P_i-P_{ref}}{\rho g}
+      f = \frac{1}{V_D} \sum_{i \in D} V_i ||\nabla {X_i} ||^n
 
   and gradient is estimated using the Green-Gauss cell-centered scheme:
 
   .. math::
-      
-      \nabla h_i = \frac{1}{V_i} 
-           \sum_{j \in \partial i} A_{ij} \boldsymbol{n_{ij}} \left[d_i h_i + (1-d_i) h_j \right]
-  
-  Required PFLOTRAN output, ``LIQUID_PRESSURE``, ``CONNECTION_IDS``, 
-  ``FACE_AREA``, ``FACE_UPWIND_FRACTION``, ``VOLUME``, ``Z_COORDINATE``, 
+
+      \nabla X_i = \frac{1}{V_i}
+           \sum_{j \in \partial i} A_{ij} \boldsymbol{n_{ij}} \left[d_i X_i + (1-d_i) X_j \right]
+
+  Required output, ``CONNECTION_IDS``,
+  ``FACE_AREA``, ``FACE_UPWIND_FRACTION``, ``VOLUME``, ``Z_COORDINATE``,
   ``FACE_NORMAL_X``, ``FACE_NORMAL_Y`` and ``FACE_NORMAL_Z``.
-  
-  
+
+
   :param ids_to_consider: the cell ids on which to compute the mean gradient
   :type ids_to_consider: iterable
   :param power: the penalizing power ``n`` above
@@ -42,58 +36,51 @@ class Head_Gradient(Base_Function):
   :param restrict_domain: an option to calculate the gradient considering only the considered cells instead considering the whole simulation. Might change the gradient calculated at the boundary of the considered cells.
   :type restrict_domain: bool
   """
-  def __init__(self, ids_to_consider="everywhere", power=1.,
-               gravity=9.8068, density=997.16, 
-               ref_pressure=101325, restrict_domain=False):
 
-    if isinstance(ids_to_consider, str) and \
-             ids_to_consider.lower() == "everywhere":
-      self.ids_to_consider = None
-    elif isinstance(ids_to_consider, np.ndarray):
-      self.ids_to_consider = ids_to_consider-1
-    else:
-      try:
-        self.ids_to_consider = np.array(ids_to_consider) -1
-      except:
-        raise ValueError("Argument 'ids_to_consider' must be a numpy array or a object that can be converted to")
-    
-    super(Head_Gradient, self).__init__()
+  deriv_var_to_skip = [
+    "CONNECTION_IDS",
+    "FACE_AREA", "FACE_UPWIND_FRACTION",
+    "VOLUME",
+    "FACE_NORMAL_X", "FACE_NORMAL_Y", "FACE_NORMAL_Z"
+  ]
+
+  def __init__(self, cell_ids, variable, power=1., restrict_domain=False):
+
+    super(Cell_Gradient, self).__init__()
+    self.ids_to_consider = cell_ids
     self.power = power
-    self.density = density
-    self.gravity = gravity
-    self.ref_pressure = ref_pressure
+    self.variable = variable.upper()
     self.restrict_domain = restrict_domain
-    
+
     #quantities derived from the input calculated one time
     self.initialized = False #a flag indicating calculated (True) or not (False)
     self.vec_con = None
-    
+
     #required for problem crafting
     self.variables_needed = [
-      "LIQUID_PRESSURE",
+      self.variable,
       "CONNECTION_IDS",
       "FACE_AREA", "FACE_UPWIND_FRACTION",
-      "VOLUME", "ELEMENT_CENTER_Z",
+      "VOLUME",
       "FACE_NORMAL_X", "FACE_NORMAL_Y", "FACE_NORMAL_Z"
     ]
-    self.name = "Head Gradient"
+    self.name = f"{self.variable} Cell Gradient"
+    self.indexes = None #request all data
+    self.cell_id_start_at = 0 #updated in place by the crafter
     return
-    
-  def set_ids_to_consider(self, x):
-    self.ids_to_consider = x-1
-    return
-    
+
+
   def set_inputs(self, inputs):
+    self.inputs = inputs
     no_bc_connections = (inputs["CONNECTION_IDS"][:,0] > 0) * (inputs["CONNECTION_IDS"][:,1] > 0)
-    self.pressure = inputs["LIQUID_PRESSURE"]
+    self.var_data = inputs[self.variable]
     self.connection_ids = inputs["CONNECTION_IDS"][no_bc_connections]-1
     self.areas = inputs["FACE_AREA"][no_bc_connections]
     self.fraction = inputs["FACE_UPWIND_FRACTION"][no_bc_connections]
     self.volume = inputs["VOLUME"]
-    self.z = inputs["ELEMENT_CENTER_Z"]
     self.normal = [inputs["FACE_NORMAL_"+c][no_bc_connections] for c in "XYZ"]
     return
-  
+
   ### COST FUNCTION ###
   def compute_head_gradient(self, head):
     """
@@ -106,38 +93,38 @@ class Head_Gradient(Base_Function):
     grad_con = self.vec_con * head_con[:,np.newaxis]
     #sum
     grad = np.zeros((len(head),3), dtype='f8') #gradient at each considered cells
-    if self.restrict_domain: 
+    if self.restrict_domain:
       for i in range(3):
         __cumsum_from_connection_to_array__(grad[:,i], self.connection_ids[:,0][self.mask_restricted],
                                             grad_con[:,i][self.mask_restricted])
         __cumsum_from_connection_to_array__(grad[:,i], self.connection_ids[:,1][self.mask_restricted],
                                             -grad_con[:,i][self.mask_restricted])
-    else: 
+    else:
       for i in range(3):
         __cumsum_from_connection_to_array__(grad[:,i], self.connection_ids[:,0],
                                             grad_con[:,i])
         __cumsum_from_connection_to_array__(grad[:,i], self.connection_ids[:,1],
                                             -grad_con[:,i])
-    
+
     grad /= self.volume[:,np.newaxis]
     return grad
-  
-  
+
+
   def get_head_gradient(self):
     """
     Return the XYZ head gradient at every cell considered
     """
     if not self.initialized: self.__initialize__()
-    head = (self.pressure-self.ref_pressure) / (self.density * self.gravity) + self.z
+    head = self.var_data
     gradXYZ = self.compute_head_gradient(head) - self.grad_correction*head[:,np.newaxis]
     return gradXYZ[self.ids_to_consider]
-  
+
   def write_head_gradient_magnitude(self, f_out="grad.h5"):
     """
     Write head gradient calculated at every cell of the (restricted) domain
     """
     if not self.initialized: self.__initialize__()
-    head = (self.pressure-self.ref_pressure) / (self.density * self.gravity) + self.z
+    head = self.var_data
     gradXYZ = self.compute_head_gradient(head) - self.grad_correction*head[:,np.newaxis]
     #write it
     import h5py
@@ -148,36 +135,36 @@ class Head_Gradient(Base_Function):
     out.create_dataset("Grad magnitude", data=np.sqrt(gradXYZ[:,0]**2+gradXYZ[:,1]**2+gradXYZ[:,2]**2))
     out.close()
     return
-    
-  
+
+
   def evaluate(self, p):
     """
     Evaluate the function
     """
     if not self.initialized: self.__initialize__()
-    head = (self.pressure-self.ref_pressure) / (self.density * self.gravity) + self.z
+    head = self.var_data
     gradXYZ = self.compute_head_gradient(head) - self.grad_correction*head[:,np.newaxis]
     #objective value
     grad_mag = np.sqrt(gradXYZ[:,0]**2+gradXYZ[:,1]**2+gradXYZ[:,2]**2)
     cf = np.sum(self.volume[self.ids_to_consider] * grad_mag[self.ids_to_consider]**self.power) / self.V_tot
     return cf
-  
-  
+
+
   ### PARTIAL DERIVATIVES ###
   def d_objective(self,var, p):
     """
     Derivative according to pressure
     """
-    if var == "LIQUID_PRESSURE":
-      #TODO: see what happen when grad_mag is null
-      dobj = np.zeros(len(self.pressure), dtype='f8')
+    #TODO: see what happen when grad_mag is null
+    dobj = np.zeros_like(self.var_data)
 
+    if var == self.variable:
       #gradient value
       n = self.power
-      head = (self.pressure-self.ref_pressure) / (self.density * self.gravity) + self.z
+      head = self.var_data
       gradXYZ = self.compute_head_gradient(head) - self.grad_correction*head[:,np.newaxis]
       grad_mag = np.sqrt(gradXYZ[:,0]**2+gradXYZ[:,1]**2+gradXYZ[:,2]**2)
-      
+
       #d_con_x_y for cell x when head at cell y is increased
       #increase of head at i on grad at i
       d_grad = self.fraction[:,np.newaxis] * self.vec_con# + \
@@ -219,21 +206,24 @@ class Head_Gradient(Base_Function):
       d_norm = np.sum(d_grad * gradXYZ, axis=1)
       d_con = d_norm * grad_mag**(n-2)
       dobj[self.ids_to_consider] += d_con[self.ids_to_consider]
-      
-      dobj *= n / self.V_tot / (self.density * self.gravity)
+
+      dobj *= n / self.V_tot
     else:
-      raise NotImplementedError()
+      dobj = super(Cell_Gradient, self).d_objective(var,p)
     return dobj
-  
+
   ### INITIALIZER FUNCTION ###
   def __initialize__(self):
     """
-    Initialize the derived quantities from the inputs that must be compute one 
+    Initialize the derived quantities from the inputs that must be compute one
     time only.
     """
     self.initialized = True
     if self.ids_to_consider is None: #by default, sum everywhere
       self.ids_to_consider = np.arange(len(self.volume))
+    else: #correct for the solver id start_at
+      self.ids_to_consider = self.ids_to_consider - self.cell_id_start_at
+
     self.V_tot = np.sum(self.volume[self.ids_to_consider])
     #correspondance between cell ids and p: not needed
     #parametrized
@@ -252,4 +242,56 @@ class Head_Gradient(Base_Function):
     head = np.ones(len(self.volume),dtype='f8')
     self.grad_correction = self.compute_head_gradient(head)
     return
-                       
+
+  @classmethod
+  def sample_instance(cls):
+    # Suppose this grid
+    # 1 - 2 - 3
+    # |   |   |
+    # 4 - 5 - 6
+    # |   |   |
+    # 7 - 8 - 9
+    connections = np.array([
+      (1, 2), (1, 4),
+      (2, 3), (2, 5),
+      (3, 6),
+      (4, 5), (4, 7),
+      (5, 6), (5, 8),
+      (6, 9),
+      (7, 8),
+      (8, 9),
+    ], dtype='i4')
+    face_normal = np.array([
+      [1,0,0], [0,-1,0],
+      [1,0,0], [0,-1,0],
+      [0,-1,0],
+      [1,0,0], [0,-1,0],
+      [1,0,0], [0,-1,0],
+      [0,-1,0],
+      [1,0,0], [1,0,0]
+    ], dtype='f8')
+    N = 9
+    M = len(connections)
+    face_normal[:,2] = np.random.random(M)
+    face_normal[:,:2] *= np.random.random((M,2))
+    face_normal /= np.linalg.norm(face_normal, axis=1)[:,None]
+    inputs = {
+      "TEST_VAR":np.random.random(N),
+      "CONNECTION_IDS":connections,
+      "FACE_AREA":np.random.random(M),
+      "FACE_UPWIND_FRACTION":np.random.random(M),
+      "VOLUME":np.random.random(N),
+      "FACE_NORMAL_X":face_normal[:,0],
+      "FACE_NORMAL_Y":face_normal[:,1],
+      "FACE_NORMAL_Z":face_normal[:,2],
+    }
+
+    N = 9
+    cell_ids = np.arange(N)
+    inst1 = cls(cell_ids, "TEST_VAR", power=1.)
+    inst1.set_inputs(inputs)
+    inst1.indexes = np.arange(0,N)
+    inst2 = cls(cell_ids, "TEST_VAR", power=2.)
+    inst2.set_inputs(inputs)
+    inst2.indexes = np.arange(0,N)
+    return [inst1,inst2]

HydrOpTop/Functions/p_weighted_sum.py
@@ -32,7 +32,13 @@ class p_Weighted_Sum(Base_Function):
         # ask the crafter the density parameter p at the same place than where the field is defined
         self.indexes = field_ids
 
-        self.variables_needed = []
+        if isinstance(self.field,str):
+            self.field_ = self.field
+            self.variables_needed = [self.field_]
+        else:
+            self.field_ = "CUSTOM_FIELD"
+            self.inputs[self.field_] = self.field
+            self.variables_needed = []
         self.linear = True
         return
     
@@ -45,7 +51,7 @@ class p_Weighted_Sum(Base_Function):
         """
         if self.vp0: p_ = 1-p
         else: p_ = p
-        cf = np.sum(self.field*np.power(p_,self.n))
+        cf = np.sum(self.inputs[self.field_]*np.power(p_,self.n))
         return cf
 
     
@@ -56,12 +62,20 @@ class p_Weighted_Sum(Base_Function):
             factor = -1.
         else:
             factor = 1.
-        res = factor * self.field * self.n * p**(self.n-1)
+        res = factor * self.inputs[self.field_] * self.n * p**(self.n-1)
         return res
 
 
+    def d_objective(self,var,p):
+        if var != self.field_:
+            return np.array([0.])
+        p_ = 1-p if self.vp0 else p
+        deriv = np.power(p_,self.n)
+        return deriv
+
+
     def output_to_user(self):
-        return {"Field":("cell",self.field_ids, self.field)}
+        return {self.field_:("cell",self.field_ids, self.inputs[self.field_])}
 
 
     @classmethod